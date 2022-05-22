7f9127657e700c8fb16cf7528b2228a2fb266654
clogerie <66565287+clogerie@users.noreply.github.com>
2022-05-22 11:57:57 -0400

    added global variables and startGame function

--- script.js
+++ script.js
@@ -2,4 +2,19 @@ var startBtn = document.getElementById("start-btn");
 var timerEl = document.getElementById("timer");
 var questionsDiv = document.getElementById("question");
 var submitBtn = document.getElementById("submit-btn");
+var time = 90;
+var timeInterval;
+
+function startGame() {
+    timeInterval = setInterval(function() {
+        timerEl.textContent = "Time remaining: " + time;
+        time--;
+        if (time === 0) {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+    console.log("Start button clicked!");
+}
+
+startBtn.addEventListener("click", startGame);
 
