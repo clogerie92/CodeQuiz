e1951fcf76b6bdb0b5ab339666a321fa1501215a
clogerie <66565287+clogerie@users.noreply.github.com>
2022-05-22 11:29:35 -0400

    html updates

---
+++ script.js
@@ -0,0 +1,5 @@
+var startBtn = document.getElementById("start-btn");
+var timerEl = document.getElementById("timer");
+var questionsDiv = document.getElementById("question");
+var submitBtn = document.getElementById("submit-btn");
+
