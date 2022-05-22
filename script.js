2e2aa491225f9b81b32856cce9120c29c2c83262
clogerie <66565287+clogerie@users.noreply.github.com>
2022-05-22 12:53:27 -0400

    added function to render questions

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 var startBtn = document.getElementById("start-btn");
 var timerEl = document.getElementById("timer");
-var questionsDiv = document.getElementById("question");
+var questionsEl = document.getElementById("questions");
+var choicesEl = document.getElementById("choices-list");
+var questionResult = document.getElementById("question-result");
 var submitBtn = document.getElementById("submit-btn");
 var time = 90;
 var timeInterval;
+var intervalID;
+var questionsIndex = 0;
+var userScore = 0;
+var questions = [
+    {
+        question: "Inside which HTML tag do you put the JavaScript?",
+        choices: ["<html>", "<js>", "<header>", "<script>"],
+        answer: "<script>"
+    },
+    {
+        question: "Which of the following is an ID selector?",
+        choices: [".", "*", "#", "$"],
+        answer: "#"
+    },
+    {
+        question: "The condidition of an if/else statement is enclosed within____.",
+        choices: ["parentheses", "curlybrace", "square brackets", "quotation marks"],
+        answer: "parentheses"
+    },
+    {
+        question: "Which of the following is a class selector?",
+        choices: [".", "$", "<class>", "#"],
+        answer: "."
+    },
+    {
+        question: "What is used to store key/value pairs?",
+        choices: ["arrays", "objects", "functions", "for loops"],
+        answer: "objects"
+    },
+   ];
+
+   function renderQuestion() {
+    questionsEl.textContent = questions[questionsIndex].question;
+   }
 
 function startGame() {
     timeInterval = setInterval(function() {
@@ -14,7 +50,9 @@ function startGame() {
         }
     }, 1000);
     console.log("Start button clicked!");
+    renderQuestion();    
 }
-
 startBtn.addEventListener("click", startGame);
+
+
 
