6a7eadf53ce52520710f6f36976f06845f6e8b35
clogerie <66565287+clogerie@users.noreply.github.com>
2022-05-24 16:45:31 -0400

    javascript updates

--- script.js
+++ script.js
@@ -19,27 +19,27 @@ var questionsIndex = 0;
 var userScore = 0;
 var questions = [
     {
-        question: "Inside which HTML tag do you put the JavaScript?",
+        question: "1: Inside which HTML tag do you put the JavaScript?",
         choices: ["<html>", "<js>", "<header>", "<script>"],
         answer: "<script>"
     },
     {
-        question: "Which of the following is an ID selector?",
+        question: "2: Which of the following is an ID selector?",
         choices: [".", "*", "#", "$"],
         answer: "#"
     },
     {
-        question: "The condidition of an if/else statement is enclosed within____.",
+        question: "3: The condidition of an if/else statement is enclosed within____.",
         choices: ["parentheses", "curlybrace", "square brackets", "quotation marks"],
         answer: "parentheses"
     },
     {
-        question: "Which of the following is a class selector?",
+        question: "4: Which of the following is a class selector?",
         choices: [".", "$", "<class>", "#"],
         answer: "."
     },
     {
-        question: "What is used to store key/value pairs?",
+        question: "5: What is used to store key/value pairs?",
         choices: ["arrays", "objects", "functions", "for loops"],
         answer: "objects"
     },
